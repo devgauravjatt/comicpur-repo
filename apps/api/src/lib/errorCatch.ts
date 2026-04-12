e939b7f8dd3e1e430ddfa2bed0bffd03e3346f47
devgauravjatt <devgauravjatt@gmail.com>
2026-04-12 06:33:24 +0530

    api: rslog add

--- apps/api/src/lib/errorCatch.ts
+++ apps/api/src/lib/errorCatch.ts
@@ -1,8 +1,10 @@
 import type { HTTPResponseError } from 'hono/types';
 import '@/lib/instrument.js';
 import * as Sentry from '@sentry/node';
+import { logger } from 'rslog';
 
 // biome-ignore lint/suspicious/noExplicitAny: <for error catching>
 export default function errorCatch(err: Error | HTTPResponseError, value: any) {
+	logger.error(err);
 	Sentry.captureException(err, value);
 }
